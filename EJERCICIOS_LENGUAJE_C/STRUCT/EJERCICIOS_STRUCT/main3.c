27031f5d60b6689da3c33155c6fd5b45e20e5fb1
DATASAGASTEGUI <datasagamadrid@gmail.com>
2025-04-08 17:31:19 +0200

    SNAPSHOT 1

---
+++ EJERCICIOS_LENGUAJE_C/STRUCT/EJERCICIOS_STRUCT/main3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+struct Persona {
+    char nombre[50];
+    int edad;
+    float estatura;
+};
+
+void mostrarPersona(struct Persona persona[], int longitud) {
+    for(int i=0; i<longitud; i++) {
+        printf("Persona : %d\n", i+1); 
+        printf("Nombre  : %s\n",   persona[i].nombre);
+        printf("Edad    : %d\n",   persona[i].edad);
+        printf("Estatura: %.2f\n", persona[i].estatura);
+    }
+
+}
+
+int main() {
+    struct Persona persona[2]; // Declarar un arreglo de tipo Persona para almacenar 2 personas
+    int i;
+    // Pedir datos para las personas y almacenarlos en el arreglo
+    for(int i=0; i<2; i++) {
+        printf("Ingrese el nombre de la persona %d: ", i+1);
+        scanf("%s", persona[i].nombre); // Leer el nombre de la persona
+        printf("Ingrese la edad de la persona %d: ", i+1);
+        scanf("%d", &persona[i].edad); // Leer la edad de la persona
+        printf("Ingrese la estatura de la persona %d: ", i+1);
+        scanf("%f", &persona[i].estatura); // Leer la estatura de la persona
+    }
+    // Mostrar los datos de las personas almacenados en el arreglo
+    mostrarPersona(persona, 2); // Llamar a la funcion para mostrar los datos de las personas   
+    
+
+    return 0; // Fin del programa
+}
